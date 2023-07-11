64645753514dbd36058d1f52d8eec73835484201
AshiqPranto <ashiqpranto@gmail.com>
2023-07-11 09:30:54 +0600

    REFACTOR V1.0

--- 1_a_CaseConversion.asm
+++ 1_a_CaseConversion.asm
@@ -1,17 +1,16 @@
-.MODEL SMALL
-.STACK 100H
-.DATA
-
-STR DB 100 DUP ('$')
-INPUT_MSG1 DB 'ENTER YOUR INPUT STRING:  $'
-OUTPUT_MSG1 DB 'Output:  $'
-NEW_LINE DB '0AH','0DH','$'
-
-.CODE
-MAIN PROC
-
-    MOV AX,@DATA
-    MOV DS,AX   
+ .MODEL SMALL
+ .STACK 100H
+ .DATA
+    
+    STR DB 100 DUP('$')
+    INPUT_MSG1 DB 'ENTER YOUR INPUT STRING:  $'
+    OUTPUT_MSG DB 'OUTPUT STRING:  $'
+    NEW_LINE DB 0AH,0DH,'$'
+ .CODE
+ 
+ MAIN PROC        
+    MOV AX, @DATA
+    MOV DS, AX
 
     ;Show input prompt
     MOV AH,9
@@ -19,50 +18,57 @@ MAIN PROC
     INT 21H 
 
     ; TAKE INPUT
+    
     MOV AH,1
-    XOR CX,CX
     XOR SI,SI
+    XOR CX,CX
+    
     INPUT:
         INT 21H
-        CMP AL,0DH
-        JE EXIT_FROM_INPUT
+        CMP AL, 0DH
+        JE EXIT_INPUT
         MOV STR[SI],AL
-        INC CX
         INC SI
-        JMP INPUT
-    EXIT_FROM_INPUT:
-
-    ;Show Output Prompt
-    LEA DX,OUTPUT_MSG1
-    MOV AH,9
+    JMP INPUT
+    EXIT_INPUT:        
+    
+    MOV AH,9   
+    
+    LEA DX, OUTPUT_MSG
     INT 21H 
+    
 
-    ;CASE CONVERSION AND OUTPUT
-    XOR SI,SI 
+    
+    MOV CX,SI
+    
+    XOR SI,SI
     LOOP_THROUGH_STR:
         MOV DL,STR[SI]
-        ;check if LOAWER case
-        CMP DL,61H
-        JL CHECK_UPPER_CASE
-        CMP DL,7AH
-        JG PRINT
-        SUB DL,20H
-        JMP PRINT
-        ; CHECK IF UPPER CASE
-        CHECK_UPPER_CASE:
-        CMP DL,41H
+
+        CMP DL, 'A'
         JL PRINT
-        CMP DL,5AH
-        JG PRINT
+        CMP DL, 'Z'
+        JG CHECK_SMALL
         ADD DL,20H
+        JMP PRINT
+
+        CHECK_SMALL:
+        CMP DL,'a'
+        JL PRINT
+        CMP DL, 'z'
+        JG PRINT
+        SUB DL, 20H
+        JMP PRINT
 
         PRINT:
         MOV AH,2
-        INT 21H 
-        
-        INC SI
-        LOOP LOOP_THROUGH_STR
-
-MAIN ENDP
-
-END MAIN
+        INT 21H
+    INC SI
+    LOOP LOOP_THROUGH_STR
+    
+    
+ 
+    
+ MAIN ENDP
+ 
+ END MAIN
